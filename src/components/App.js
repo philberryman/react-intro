a2b092545e0972ba6f57a38d1e12a7010ba878e1
dmitrigrabov <d.grabov@gmail.com>
2018-09-23 20:58:34 +0100

    update initial App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1 className="main-heading">Hello, world!</h1>
+        <p>Welcome to our first React component</p>
+      </div>
+    );
+  }
+}
+
+export default App;
